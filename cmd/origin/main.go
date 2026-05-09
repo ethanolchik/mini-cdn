7662f59c4f3cfa08dc7e8e4632d32b969c1de67b
Ethan Olchik <eitan.olchik@gmail.com>
2026-05-09 12:58:20 +0100

    feat: Implement LRU cache with TTL

--- cmd/origin/main.go
+++ cmd/origin/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello from origin server! You requested: %s", r.URL.Path)
+		fmt.Fprintf(w, "Hello from origin server %s! You requested: %s\n", r.Host, r.URL.Path)
 	})
 	log.Println("Origin server listening on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
---
+++ internal/cache/cache.go
@@ -0,0 +1,173 @@
+package cache
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type node struct {
+	key       string
+	val       CacheEntry
+	next      *node
+	prev      *node
+	timestamp time.Time
+}
+
+type CacheEntry struct {
+	StatusCode int
+	Headers    http.Header
+	Body       []byte
+}
+
+// LRU Cache
+// Implemented as a doubly-linked list, where elements are inserted at the tail and removed from the head.
+type Cache struct {
+	Capacity int              // The capacity of the cache
+	Size     int              // The number of elements currently in cache
+	cache    map[string]*node // A hashmap where keys are http method + url path + query string, values are nodes
+	head     *node
+	tail     *node
+
+	maxTTLSeconds float64
+
+	mu sync.Mutex
+}
+
+type CacheMissError struct {
+	key string
+}
+
+func (cme *CacheMissError) Error() string {
+	return fmt.Sprintf("Cache miss on key %s", cme.key)
+}
+
+func New(capacity int, maxTTLSeconds float64) Cache {
+	if capacity <= 0 {
+		panic("Cache capacity must be greater than 0")
+	}
+
+	// Sentinel
+	head := &node{
+		next: nil,
+		prev: nil,
+	}
+	tail := &node{
+		next: nil,
+		prev: nil,
+	}
+
+	head.next = tail
+	tail.prev = head
+
+	return Cache{
+		Capacity:      capacity,
+		Size:          0,
+		cache:         make(map[string]*node),
+		head:          head,
+		tail:          tail,
+		maxTTLSeconds: maxTTLSeconds,
+	}
+}
+
+func (c *Cache) push_back(entry *node) {
+	// If the entry is already in the list, we should remove it first before pushing it back to the tail.
+	if entry.prev != nil && entry.next != nil {
+		entry.prev.next = entry.next
+		entry.next.prev = entry.prev
+	}
+
+	// Insert the entry at the tail of the list.
+	c.tail.prev.next = entry
+	entry.prev = c.tail.prev
+	c.tail.prev = entry
+	entry.next = c.tail
+
+	if _, ok := c.cache[entry.key]; !ok {
+		c.cache[entry.key] = entry
+	}
+}
+
+// Remove an element from the cache by its key
+func (c *Cache) remove(key string) error {
+	if entry, ok := c.cache[key]; ok {
+		entry.prev.next = entry.next
+		entry.next.prev = entry.prev
+
+		delete(c.cache, key)
+		c.Size--
+		return nil
+	} else {
+		return &CacheMissError{key}
+	}
+}
+
+// Evict the least recently used element from the cache, which is the one at the head of the list.
+func (c *Cache) evict() {
+	if c.Size == 0 {
+		return
+	}
+
+	// Remove the least recently used element, which is the one at the head of the list.
+	lru := c.head.next
+	c.head.next = lru.next
+	lru.next.prev = c.head
+
+	delete(c.cache, lru.key)
+	c.Size--
+}
+
+// Get a value in cache from its key.
+// Key is in the form: HTTP Method + URL Path + Query String.
+// Returns error on cache miss.
+func (c *Cache) Get(key string) (CacheEntry, error) {
+	now := time.Now()
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.Size == 0 {
+		return CacheEntry{}, &CacheMissError{key: key}
+	}
+
+	if entry, ok := c.cache[key]; ok {
+		// If the cache entry has expired, we treat it as a cache miss
+		if now.Sub(entry.timestamp).Abs().Seconds() >= c.maxTTLSeconds {
+			c.remove(key)
+			return CacheEntry{}, &CacheMissError{key: key}
+		}
+
+		c.push_back(entry)
+
+		return entry.val, nil
+	}
+
+	// If we didn't find it in the cache, we return cache miss
+	return CacheEntry{}, &CacheMissError{key: key}
+}
+
+func (c *Cache) Put(key string, value CacheEntry) {
+	now := time.Now()
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if _, ok := c.cache[key]; ok {
+		// If we get a cache hit, we should update the value and timestamp
+		c.cache[key].val = value
+		c.cache[key].timestamp = now
+	} else {
+		if c.Size == c.Capacity {
+			c.evict()
+		}
+		c.cache[key] = &node{
+			key:       key,
+			val:       value,
+			next:      nil,
+			prev:      nil,
+			timestamp: now,
+		}
+		c.Size++
+	}
+
+	c.push_back(c.cache[key])
+}
